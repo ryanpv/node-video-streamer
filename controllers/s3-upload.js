0566bf8c4100cbdd0b94c9f6fd6c04a793d04f14
Ryan <ryanpvoong@gmail.com>
2023-08-28 22:47:00 -0400

    created s3 upload function

--- controllers/s3-upload.js
+++ controllers/s3-upload.js
@@ -4,7 +4,33 @@ const fs = require("fs");
 
 const s3upload = async (req, res) => {
   try {
-    const
+    fs.readdir("/server/videos", (err, files) => {
+      if (err) {
+        console.log("readdir error: ", err);
+        process.exit(1)
+      }
+
+      files.forEach(async (file) => {
+        const uploadFile = new Upload({
+          client: new S3Client,
+          params: {
+            Bucket: process.env.S3_BUCKET,
+            Key: `${ file }-key`,
+            Body: file,
+          },
+          tags: [],
+          leavePartsOnError: false,
+        });
+
+        uploadFile.on("httpUploadProgress", (progress) => {
+          console.log("progress: ", progress);
+        });
+
+        await uploadFile.done();
+      });
+    });
+  } catch (error) {
+    console.log("Error with s3 upload", error)
   }
 }
 
