e6361a40f5b63b070cf436991594a0c59238c6c0
Ryan <ryanpvoong@gmail.com>
2023-08-28 22:37:15 -0400

    initial commit

---
+++ controllers/s3-upload.js
@@ -0,0 +1,10 @@
+const { S3Client } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
+const fs = require("fs");
+
+const s3upload = async (req, res) => {
+  try {
+    const
+  }
+}
+
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config({ path: "./config.env"});
+const app = express();
+const PORT = 9191
+
+app.get("/", (req, res) => {
+  res.send("HomePage")
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port=${ PORT }`)
+});
