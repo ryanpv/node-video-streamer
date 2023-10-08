0cbec738e99b5f511a56d1cc395dd2c71ecc79ed
Ryan <ryanpvoong@gmail.com>
2023-10-08 12:17:36 -0400

    tracking test variables

--- controllers/getSIgnedUrl.js
+++ controllers/getSIgnedUrl.js
@@ -1,15 +1,24 @@
 const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
+const url = require("url");
+
 
 const s3SignedUrl = async (req, res) => {
   const item = getSignedUrl({
-    url: "https://d2yxw8mz314xgc.cloudfront.net/user1_rain.mp4.m3u8",
+    // url: "https://d2yxw8mz314xgc.cloudfront.net/user1_rain.mp4.m3u8",
+    url: "https://d1hiuuwq8197zo.cloudfront.net/lambda_test?media-request=user1_rain.mp4.m3u8",
+    // url: "https://d1hiuuwq8197zo.cloudfront.net/lambda_test/user1_rain.mp4.m3u8", // URI for test distro and .m3u8 path pattern
     dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24),
     privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
     keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
     // region: "us-east-1"
   });
 
+  const urlQueryParams = url.parse(item, true).query
+
+  const newUrl = `${ item }&Expires-PREFIX=${ urlQueryParams.Expires }&Key-Pair-Id-PREFIX=${ urlQueryParams['Key-Pair-Id'] }&Signature-PREFIX=${ urlQueryParams.Signature }`
+
   console.log("result", item)
+  console.log("parsed url: ", url.parse(newUrl, true).query) // returns separation of query params
   res.send({ cloudfrontUrl: item })
 };
 
